caf4e7720bd95c731b9b325bfb81956701a00782
SadikRa <sadikrahman494@gmail.com>
2024-09-22 21:22:49 +0600

    nav added done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <div className='container mx-auto'>
+   <RouterProvider router={router} />
+   </div>
   </StrictMode>,
 )
