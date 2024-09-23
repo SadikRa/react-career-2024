b02c1b90b8b7146dfe00a58b12eada3764dba7aa
SadikRa <sadikrahman494@gmail.com>
2024-09-23 12:09:54 +0600

    job card added done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,15 @@
 import Banner from "../Banner/Banner";
+import CategoryList from "../CategoryList/CategoryList";
+import FeaturedJobs from "../FeaturedJobs/FeaturedJobs";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h1>this is home</h1>
+            {/* <h1>this is home</h1>
+             */}
+              <CategoryList></CategoryList>
+             <FeaturedJobs></FeaturedJobs>
             
         </div>
     );
