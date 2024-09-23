8182c3cff0dd8c07c48442d8c689f2982ae4abf8
SadikRa <sadikrahman494@gmail.com>
2024-09-23 16:46:43 +0600

    done

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -13,6 +13,7 @@ const FeaturedJobs = () => {
   return (
     <div>
       <div className="text-center">
+        
         <h2 className="text-6xl">Featured Jobs</h2>
         <p>
           Explore thousands of job opportunities with all the information you
